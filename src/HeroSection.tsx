71c04aa223eb038f975af74ceaed21d4cf63fa49
ahsan2649 <ahsan2649@outlook.com>
2023-12-22 05:04:56 +0100

    Create HeroSection.tsx

---
+++ src/HeroSection.tsx
@@ -0,0 +1,7 @@
+export function HeroSection() {
+  return <div id='hero'>
+    <div id="hero-name"><h1>Ahsan</h1></div>
+    <div id='cursor'></div>
+    <div id="hero-caption"><h2>Programming<br />Polyglot</h2></div>
+  </div>;
+}
