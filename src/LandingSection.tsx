60f757ccfb6be81183d09089936a2af7dff93e33
ahsan2649 <ahsan2649@outlook.com>
2023-12-22 05:51:09 +0100

    Changing category in landing section with time

--- src/LandingSection.tsx
+++ src/LandingSection.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react";
+
 export function LandingSection() {
+  const categories=["Games", "Websites", "Apps", "Experiments"]
+
+  
+  const [index, setIndex] = useState(0);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIndex((index+1)%categories.length);
+    }, 5000);
+    
+    return () => clearTimeout(timer);
+  });
   return <div>
     <h2>I'm Ahsan and I make</h2>
-    <h2>Games</h2>
+    <h2>{categories[index]}</h2>
   </div>;
 }
