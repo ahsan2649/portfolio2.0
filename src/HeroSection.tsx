4d714eec6c8930f350c5000cfb01f61b5561b8d5
ahsan2649 <ahsan2649@outlook.com>
2023-12-23 05:07:13 +0100

    Hero video background

--- src/HeroSection.tsx
+++ src/HeroSection.tsx
@@ -1,5 +1,10 @@
+import circlesVideo from "./assets/Shattered Circles.mp4";
+
 export function HeroSection() {
   return <div id='hero-section'>
+    <div id="hero-bg">
+      <video src={circlesVideo} autoPlay loop muted></video>
+    </div>
     <div id="hero-name"><h1>Ahsan</h1></div>
     <div id='cursor'></div>
     <div id="hero-caption"><h2>Programming<br/>Polyglot</h2></div>
