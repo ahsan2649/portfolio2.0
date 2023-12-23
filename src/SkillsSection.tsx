d66e8b26c783d6cbe983361604c276583b63d55c
ahsan2649 <ahsan2649@outlook.com>
2023-12-23 12:14:00 +0100

    logos in skills section

--- src/SkillsSection.tsx
+++ src/SkillsSection.tsx
@@ -1,69 +1,51 @@
 import { CSSProperties } from "react";
+import UnityLogo from "./assets/SVG/Unity.svg";
+import UnrealLogo from "./assets/SVG/Unreal.svg";
+import CSLogo from "./assets/SVG/CS.svg";
+import CPPLogo from "./assets/SVG/CPP.svg";
+import CLogo from "./assets/SVG/C.svg";
+import CSSLogo from "./assets/SVG/CSS.svg";
+import AngularLogo from "./assets/SVG/Angular.svg";
+import ReactLogo from "./assets/SVG/React.svg";
+import HTMLLogo from "./assets/SVG/HTML.svg";
+import ShadertoyLogo from "./assets/SVG/Shadertoy.svg";
+import JSLogo from "./assets/SVG/JS.svg";
+import TSLogo from "./assets/SVG/TS.svg";
+import WindowsLogo from "./assets/SVG/Windows.svg";
+import ArduinoLogo from "./assets/SVG/Arduino.svg";
+import DotNETLogo from "./assets/SVG/DotNET.svg";
+import LinuxLogo from "./assets/SVG/Linux.svg";
+import FlutterLogo from "./assets/SVG/Flutter.svg";
+import DartLogo from "./assets/SVG/Dart.svg";
+
+
 
 export function SkillsSection() {
-  
-  const style:CSSProperties = {
-    display:"grid",
-    gridTemplateRows:"1fr",
-    gridTemplateColumns:"1fr 1fr 1fr 1fr"
-  }
 
   return <div id="skills-section">
     <h2>My Skills Include:</h2>
-    <div style={style}>
-    <div>
-      <h3>Games</h3>
-      <ul>
-        <li>Unity</li>
-        <li>Unreal Engine</li>
-      </ul>
-      <ul>
-        <li>C#</li>
-        <li>C++</li>
-        <li>HLSL</li>
-      </ul>
-    </div>
-    <div>
-      <h3>Websites</h3>
-      <ul>
-        <li>React</li>
-        <li>Angular</li>
-        <li>Framer Motion</li>
-        <li>React Spring</li>
-      </ul>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>TS</li>
-        <li>JS</li>
-      </ul>
-    </div>
-    <div>
-      <h3>Apps</h3>
-      <ul>
-        <li>Windows API</li>
-        <li>Windows App SDK (WinUI)</li>
-        <li>.NET</li>
-        <li>Flutter</li>
-      </ul>
-      <ul>
-        <li>C#</li>
-        <li>C++</li>
-        <li>Dart</li>
-      </ul>
-    </div>
-    <div>
-      <h3>Misc.</h3>
-      <ul>
-        <li>Arduino</li>
-        <li>Shadertoy</li>
-      </ul>
-      <ul>
-        <li>C</li>
-        <li>GLSL</li>
-      </ul>
-    </div>
+    <div id="skill-table">
+    <SkillTable category="Games" languages={[CPPLogo, CSLogo]} frameworks={[UnrealLogo, UnityLogo]}/>
+    <SkillTable category="Websites" languages={[HTMLLogo, CSSLogo, TSLogo, JSLogo]} frameworks={[ReactLogo, AngularLogo]}/>
+    <SkillTable category="Apps" languages={[CPPLogo, CSLogo, DartLogo]} frameworks={[WindowsLogo, DotNETLogo, FlutterLogo]}/>
+    <SkillTable category="Misc" languages={[CLogo]} frameworks={[ArduinoLogo, ShadertoyLogo, LinuxLogo]}/>
     </div>
     
+  </div>;
+}
+
+function SkillTable(props:{category:string, frameworks?:string[], languages?:string[]}) {
+  return <div className="skill-category">
+    <h3>{props.category}</h3>
+    <div className="frameworks">
+      {props.frameworks?.map(
+        (e,i)=><img src={e} key={i}/>
+      )}
+    </div>
+    <div className="languages">
+      {props.languages?.map(
+        (e,i)=><img src={e} key={i}/>
+      )}
+    </div>
   </div>;
 }
